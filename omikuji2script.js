506ff04e1b70550075ee02f58a1e33fe9cc91e18
testjinja <145633831+testjinja@users.noreply.github.com>
2023-10-21 23:33:46 +0900

    omikuji2script.js を更新

--- omikuji2script.js
+++ omikuji2script.js
@@ -217,8 +217,10 @@ document.getElementById("retry-button").addEventListener("click", function() {
     switchCount = 0;
 
     // イラストNo.指定部分を再表示する
-    document.querySelector(".centered-container").style.display = "";
-
+    const centeredContainers = document.querySelectorAll(".centered-container");
+    centeredContainers.forEach(container => {
+        container.style.display = "";
+    });
     // おめでとう画面を非表示にする
     document.getElementById("congratulations-modal").style.display = "none";
 });
