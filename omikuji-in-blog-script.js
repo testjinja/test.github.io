5ddd84112d88ae698bb61ee5c38d7ed00561d7fe
testjinja <145633831+testjinja@users.noreply.github.com>
2023-12-06 20:12:24 +0900

    Update omikuji-in-blog-script.js

--- omikuji-in-blog-script.js
+++ omikuji-in-blog-script.js
@@ -2,3 +2,13 @@ window.addEventListener("load", function() {
     var iframe = document.getElementById("your-iframe-id");
     iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px";
 });
+
+document.getElementById('omikuji1-btn').addEventListener('click', function() {
+    document.getElementById('omikuji1-content').style.display = 'block';
+    document.getElementById('omikuji2-content').style.display = 'none';
+});
+
+document.getElementById('omikuji2-btn').addEventListener('click', function() {
+    document.getElementById('omikuji1-content').style.display = 'none';
+    document.getElementById('omikuji2-content').style.display = 'block';
+});
