b16511e809268392a108db73d136bedcbdd59f2a
testjinja <145633831+testjinja@users.noreply.github.com>
2023-12-06 21:26:13 +0900

    Update omikuji-in-blog-script.js

--- omikuji-in-blog-script.js
+++ omikuji-in-blog-script.js
@@ -12,3 +12,23 @@ document.getElementById('omikuji2-btn').addEventListener('click', function() {
     document.getElementById('omikuji1-content').style.display = 'none';
     document.getElementById('omikuji2-content').style.display = 'block';
 });
+
+function openOmikuji(evt, omikujiName) {
+  var i, tabcontent, tablinks;
+
+  // すべてのタブコンテンツを非表示にする
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // すべてのタブリンクを非アクティブにする
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // クリックされたタブのコンテンツを表示し、タブをアクティブにする
+  document.getElementById(omikujiName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
